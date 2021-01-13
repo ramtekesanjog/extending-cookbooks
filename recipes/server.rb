f8d797aa9c06e806e713655cd48f5855521ba079
Sanjog Ramteke <ramteke.sanjog@gmail.com>
2021-01-13 13:23:26 +0000

    my iis file

---
+++ recipes/server.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: myiis
+# Recipe:: server
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+powershell_script 'Install IIS' do
+    code 'Add-WindowsFeature Web-Server'
+end
+ file 'C:\inetpub\wwwroot\Default.htm'do
+  content '<h1>Hello, World!</h1>'
+end
+
+service 'w3svc' do
+    action [:enable, :start]
+end
